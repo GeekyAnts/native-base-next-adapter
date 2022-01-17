6dea21d05df4fc2bf6ac138316a050af943c056d
Vidhi Kataria <vidhikataria@Vidhi-MacBook-Pro.local>
2022-01-17 12:47:25 +0530

    initial commit

---
+++ document.d.ts
@@ -0,0 +1,15 @@
+import NextDocument from "next/document";
+export declare const style: string;
+
+export declare function getInitialProps({
+  renderPage,
+}: {
+  renderPage: any;
+}): Promise<any>;
+
+export declare class Document extends NextDocument {
+  static getInitialProps: typeof getInitialProps;
+  render(): JSX.Element;
+}
+
+export default Document;
