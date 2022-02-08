c5df477daa96866899ed08273ae491ca752ab7ec
Vidhi Kataria <GeekyAnts@Vidhi-Kataria-B453-MacBook-Air.local>
2022-02-08 14:39:55 +0530

    styles added

--- document.js
+++ document.js
@@ -4,10 +4,36 @@ import { AppRegistry } from "react-native-web";
 
 // Force Next-generated DOM elements to fill their parent's height
 const normalizeNextElements = `
+  html, body, #__next {
+    width: 100%;
+    /* To smooth any scrolling behavior */
+    -webkit-overflow-scrolling: touch;
+    margin: 0px;
+    padding: 0px;
+    /* Allows content to fill the viewport and go beyond the bottom */
+    min-height: 100%;
+  }
   #__next {
-    display: flex;
+    flex-shrink: 0;
+    flex-basis: auto;
     flex-direction: column;
+    flex-grow: 1;
+    display: flex;
+    flex: 1;
+  }
+  html {
+    scroll-behavior: smooth;
+    -webkit-text-size-adjust: 100%;
     height: 100%;
+  }
+  body {
+    display: flex;
+    overflow-y: auto;
+    overscroll-behavior-y: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -ms-overflow-style: scrollbar;
   }
 `;
 
